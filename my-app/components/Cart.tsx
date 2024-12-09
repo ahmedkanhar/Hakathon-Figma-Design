9648e72b069dc231fa28d4e8f14e1dd7e3f1dd42
ahmedkanhar <ahmedalikanhar777@gmail.com>
2024-12-09 23:38:43 +0500

    cart fully styled

--- my-app/components/Cart.tsx
+++ my-app/components/Cart.tsx
@@ -85,14 +85,14 @@ export default function Cart({
 
           {/* Fixed Bottom Buttons */}
           <div className="mt-6 bg-white">
-            <div className="fixed right-0 bottom-4 bg-white flex items-center h-[40px] justify-between gap-4 sm:gap-8 w-full sm:w-[340px] mx-auto">
+            <div className="fixed right-12 bottom-4 bg-white flex items-center h-[40px] justify-between gap-4 sm:gap-8 w-full sm:w-[340px] mx-auto">
               
               <Link href="/cart">
                 <button className="w-[131px] h-[31px] py-2 px-6 border-2 border-black text-black rounded-full flex items-center justify-center hover:bg-blue-500 hover:text-white transition">
                   View Cart
                 </button>
               </Link>
-
+              
               <Link href="/checkout">
                 <button className="w-[131px] h-[31px] py-2 px-6 border-2 border-black text-black rounded-full flex items-center justify-center hover:bg-green-500 hover:text-white transition">
                   Checkout
