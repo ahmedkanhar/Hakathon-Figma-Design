e1b0f9f227a3e3b920548275f9279d633430d6db
ahmedkanhar <ahmedalikanhar777@gmail.com>
2025-01-22 05:38:56 +0500

    deplyment error solved

--- my-app/components/Auth.tsx
+++ my-app/components/Auth.tsx
@@ -70,7 +70,7 @@ const Auth = () => {
         {/* Bottom Section */}
         <div className="mt-8 text-center text-gray-600">
           <p>
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <a
               href="/signup"
               className="text-blue-600 hover:underline font-medium"
